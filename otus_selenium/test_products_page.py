7e24c460e706b9884b35c9648b103352c4ab0f2b
Yury <dopchar@gmail.com>
2019-06-19 09:17:58 +0500

    adding to homework 11

--- otus_selenium/test_products_page.py
+++ otus_selenium/test_products_page.py
@@ -53,14 +53,14 @@ class TestProductPage:
     # @pytest.mark.usefixtures('add_images')
     # @pytest.mark.usefixtures('action_with_image')
     @pytest.mark.xfail(condition=lambda: True, reason='oops!')
-    def test_add_three_images(self, driver):
-        """Add images"""
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, 'image3.jpg')
-        input_manager = driver.find_element_by_css_selector('button-upload')
-        # driver.find_element_by_id('button-upload').click()
-        # input_manager = driver.find_element_by_id('//*[@id="form-upload"]')
-        input_manager.send_keys(filename)
+    # def test_add_three_images(self, driver):
+    #     """Add images"""
+    #     dirname = os.path.dirname(__file__)
+    #     filename = os.path.join(dirname, 'image3.jpg')
+    #     input_manager = driver.find_element_by_css_selector('button-upload')
+    #     # driver.find_element_by_id('button-upload').click()
+    #     # input_manager = driver.find_element_by_id('//*[@id="form-upload"]')
+    #     input_manager.send_keys(filename)
 
     @pytest.mark.skipif('2+2 != 5', reason='Sorry!')
     def test_delete_images(self):
