be2f79897a6620cbd2a1243244126949a39a6b47
Yury <dopchar@gmail.com>
2019-06-27 22:10:46 +0500

    homework 23

--- ssh_test/my_paramiko.py
+++ ssh_test/my_paramiko.py
@@ -1,13 +1,29 @@
+"""Open SSH"""
 import paramiko
 
-host = 'localhost'
-name = 'admin'
-secret = 'admin'
-port = 22
+from otus_selenium.go_away import HOST, NAME, SECRET, PORT
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(hostname=host, username=name, password=secret, port=port)
-stdin, stdout, stderr = client.exec_command('ls -l')
-data = stdout.read() + stderr.read()
-client.close()
+
+def connect_to_paramiko_and_stop(command):
+    """Function"""
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=HOST, username=NAME, password=SECRET, port=PORT)
+    stdin, stdout, stderr = client.exec_command('sudo service apache2 ' + command + '')
+    data = stdout.read() + stderr.read()
+    client.close()
+    return data
+
+
+def test_stop_apache_server():
+    """Stop server"""
+    data = connect_to_paramiko_and_stop('stop')
+    assert 'active' not in str(data)
+
+
+def test_start_apache_server():
+    """Start and check status"""
+    connect_to_paramiko_and_stop('start')
+    data = connect_to_paramiko_and_stop('status')
+    assert 'active' in str(data)
+
